a6c2b0c098a6db0401e3099916bc45e982fee260
Rikachanwa <rikachanwa@gmail.com>
2024-02-14 08:20:34 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,5 +1,11 @@
-// 変数の宣言・値の代入
-let bmiWeight = 68
-let bmiHeight = 1.7
+// 体重と身長の変数の宣言と代入
 
-console.log(bmiWeight / (bmiHeight * bmiHeight));
+let bmiWeight = 68;
+let bmiHeight = 1.7;
+
+// BMIの定数の宣言と代入
+
+let bmi = bmiWeight / (bmiHeight * bmiHeight);
+
+// コンソールログの出力
+console.log(bmi);
